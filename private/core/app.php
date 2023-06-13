748e022e8d32936fe9fadb42c1db1acfff2e64e7
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-06-13 21:34:51 +0200

    App Class return explode()

--- private/core/app.php
+++ private/core/app.php
@@ -12,7 +12,17 @@
 
     public function __construct()
     {
-        echo "construct";
+        echo "<pre>";
+        print_r($this->getURL());
+    }
+
+    private function getURL()
+    {
+        // echo "<pre>";
+        // print_r($_GET);
+        return explode("/", filter_var(trim($_GET['url'],"/")),FILTER_SANITIZE_URL);
+        // http://localhost:8080/Quick_Programming/public/students/subject/123
+        // Array ( [0] => students [1] => subject [2] => 123 )
     }
  }
  
