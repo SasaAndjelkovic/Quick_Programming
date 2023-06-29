87b3169cbb44d60c935ba68be5ab6a54e564ce29
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-06-29 23:08:21 +0200

    print_r database object

--- PHP Object Oriental/classes_instances.php
+++ PHP Object Oriental/classes_instances.php
@@ -1,25 +1,17 @@
 <?php
 
-//blueprint / mold / pattern / factory for objects
-class Product{
-   
-    function __construct($var){
-        echo $var;
-    }
-}
-
 class Database{
-    public $hostname = "";
-    public $dbname = "";
-    function __construct($hostname, $dbname){
-    //function __construct($hostname = "default", $dbname = ""default){
-        //mora u konstrakt zbog runtime
-        $this->hostname = $hostname;
-        $this->dbname = $dbname;
-
-        echo "$hostname $dbname";
+    public $hostname = "localhost";
+    public $dbname = "mydb";
+    function __construct($hostname = "localhost", $dbname = "mydb"){
+ 
     }
+    
 }
 
-$book = new Product("za konstrukt");//instantiation
-$db = new Database("localhost", "mydb");
+$db = new Database();
+$db->name = "some name";
+
+print_r($db);
+//Database Object ( [hostname] => localhost [dbname] => mydb [name] => some name )
+echo $db->name;
