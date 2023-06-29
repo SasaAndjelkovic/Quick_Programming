79df2417df5a97368b29f70192257f4dfbdefb89
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-06-29 23:47:22 +0200

    dinamicnost u __set()

--- PHP Object Oriental/classes_instances.php
+++ PHP Object Oriental/classes_instances.php
@@ -6,12 +6,18 @@ class Database{
     function __construct($hostname = "localhost", $dbname = "mydb"){
  
     }
+    public function __set($name, $value){
+        /*$a = "variable";
+        $$a = 1; dinamicno
+        echo $variable;*/
+        $this->$name = $value;
+    }
     
 }
 
 $db = new Database();
-$db->name = "some name";
+//$db->name = "some name"; //ispis setting... zato sto se aktivira defoltna magic funk __set()
+$db->password = "some password";
 
-print_r($db);
-//Database Object ( [hostname] => localhost [dbname] => mydb [name] => some name )
-echo $db->name;
+//echo $db->name;
+echo $db->password;
