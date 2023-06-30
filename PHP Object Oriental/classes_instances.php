de8feae4f23d0c88af4d675852085f97d79411bc
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-06-30 23:29:44 +0200

    get data

--- PHP Object Oriental/classes_instances.php
+++ PHP Object Oriental/classes_instances.php
@@ -12,16 +12,46 @@ class Database{
     }
 
     public function __get($name){
+
+        //return get_data();  // Uncaught Error: Call to undefined function get_data()
+        return $this->get_data(); 
+    }   
+
+    private function get_data(){
         $arr = array();
         $arr['id'] = '1';
         $arr['name'] = 'john';
         $arr['age'] = '24';
         $arr['gender'] = 'male';
 
-        return $arr[$name];
-    }   
+        $rows[] = $arr;
+        $arr = array();
+        $arr['id'] = '2';
+        $arr['name'] = 'mary';
+        $arr['age'] = '20';
+        $arr['gender'] = 'female';
+
+        $rows[] = $arr;
+        $arr = array();
+        $arr['id'] = '3';
+        $arr['name'] = 'peter';
+        $arr['age'] = '34';
+        $arr['gender'] = 'male';
+
+        $rows[] = $arr;
+        $arr = array();
+        $arr['id'] = '4';
+        $arr['name'] = 'jack';
+        $arr['age'] = '84';
+        $arr['gender'] = 'male';
+
+        $rows[] = $arr;
+        
+        return $rows;
+    }
 }
 
 $db = new Database();
 
-echo $db->gender;
+echo "<pre>";
+print_r($db->gender);
