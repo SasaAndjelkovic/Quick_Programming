8b218fdee87e4fcc8a21e0a65ebb652d6b914c0f
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-07-01 21:36:52 +0200

    overriding

--- PHP Object Oriental/inheritance.php
+++ PHP Object Oriental/inheritance.php
@@ -1,52 +1,25 @@
 <?php
 
 class Database{
-    static public $number = 0;
-    function __construct(){
-      
-        self::$number += 10;
-        echo "database constructor<br>";
-    }
-    protected function db_connect(){
-        echo "from db_connect function<br>";
-    }
-
-    protected function db_read(){
-        echo "from db_read function<br>";
-    }
-
-    public function db_write(){
-        echo "from db_write function<br>";
-    }
+   public function show(){
+        echo "from database class<br>";
+   }
 }
 
 class Product extends Database{
-    function __construct(){
-        parent::__construct();
-        self::$number += 10;
-        echo "product constructor<br>";
+    public function show(){
+        parent::show();
+        echo "from product class<br>";
     }
-    public function new_product(){
-        echo "from product class function<br>";
-        //$this->db_connect();
+
+    public function myfunc(){
+        $this->show();
     }
 }
 
-class User extends Product{
-    function __construct(){
-        parent::__construct();
-        self::$number += 10;
-        echo "user constructor<br>";
-    }
-    public function dostuff(){
-        $this->db_read();
-    }
-}
+$a = new Product();
 
-$me = new User(); //instantiation
-//$me->dostuff();
-echo User::$number;
-// database constructor
-// product constructor
-// user constructor
-// 30
+$a->myfunc();
+// from database class
+// from product class
+
